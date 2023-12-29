6720d4f5223394da39eabc445e0fbaa474b01ab4
Drishti Singh <drishtisingh0925@gmail.com>
2023-12-29 20:59:56 +0530

    edited json-server.js

--- netlify/functions/json-server.js
+++ netlify/functions/json-server.js
@@ -1,7 +1,7 @@
 // netlify/functions/json-server.js
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('data/db.json');
+const router = jsonServer.router(path.join(__dirname, '..', '..', 'data', 'db.json'));
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
